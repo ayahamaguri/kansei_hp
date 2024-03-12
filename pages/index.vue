a422cd9b358ddd95d2a13e559f4e5ded69e71fc4
ayahamaguri <aya.hamaguchi@consallink.onmicrosoft.com>
2024-03-12 14:18:31 +0000

    feat:画像変更、下から上に動くアニメーション追加、レイアウト変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,39 @@
   <div class="background-image-container">
     <v-container class="fill-height">
       <v-responsive class="align-center text-center fill-height">
-        <div v-if="isMobile" class="py-5" />
-        <img
-          v-if="isMobile"
-          height="150"
-          src="@/assets/logo_title_msg_isMobile3.png"
-        />
         <!-- <div v-if="isMobile" class="py-5" /> -->
-        <img
-          v-else
-          height="300"
-          width="1200"
-          src="@/assets/logo_title_msg3.svg"
-        />
+        <!-- <img
+          v-if="isMobile"
+          class="slide-up-animation"
+          height="100"
+          src="@/assets/logo_title_msg_isMobile3.png"
+        /> -->
+        <div v-if="isMobile" class="vertical-stack">
+          <img
+            class="slide-up-animation"
+            width="200"
+            src="@/assets/message.png"
+          />
+          <img height="100" src="@/assets/starLogo.png" />
+          <img
+            class="slide-up-animation"
+            height="50"
+            width="200"
+            src="@/assets/companyName.png"
+          />
+        </div>
+        <div v-else class="image-container">
+          <img class="slide-up-animation" src="@/assets/message.png" />
+          <div class="bottom-row">
+            <img height="150" src="@/assets/starLogo.png" />
+            <img
+              class="slide-up-animation"
+              height="150"
+              width="600"
+              src="@/assets/companyName.png"
+            />
+          </div>
+        </div>
       </v-responsive>
     </v-container>
   </div>
@@ -26,9 +46,9 @@ export default {
   name: 'IndexPage',
 
   setup() {
-    const isMobile = ref(window.innerWidth <= 1100)
+    const isMobile = ref(window.innerWidth <= 1250)
     const handleResize = () => {
-      isMobile.value = window.innerWidth <= 1100
+      isMobile.value = window.innerWidth <= 1250
     }
     onMounted(() => {
       window.addEventListener('resize', handleResize)
@@ -67,5 +87,38 @@ export default {
 
 .custom-font {
   font-family: 'CustomFont', sans-serif; /* 'sans-serif'はフォントが読み込まれない場合のフォールバックフォント */
+}
+
+.slide-up-animation {
+  animation: slideUp 3s ease;
+  /* opacity: 0; /* 初期状態では透明にしておく */
+}
+
+@keyframes slideUp {
+  from {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.image-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 中央揃え */
+}
+
+.bottom-row {
+  display: flex;
+  justify-content: space-around; /* 画像の間隔を均等に配置 */
+}
+
+.vertical-stack {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 中央揃え */
 }
 </style>
